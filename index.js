7ceec40704ecde8ef6ba8d1178de62a2a70fcb7e
osakanakuwaenai <83628516+osakanakuwaenai@users.noreply.github.com>
2022-07-07 19:36:47 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,45 @@
+/*var naver_i = document.querySelector(".naver_i");
+var naver_i_root = document.querySelector(".naver_i_root");
+var naver_ii = document.querySelector(".naver_ii");
+var naver_ii_root = document.querySelector(".naver_ii_root");
+var naver_iii = document.querySelector(".naver_iii");
+var naver_iii_root = document.querySelector(".naver_iii_root");
+
+
+
+naver_i.addEventListener('mouseleave', function() {
+  naver_i.classList.toggle("is-hidden")
+}, false);
+naver_i_root.addEventListener('mouseleave', function() {
+  naver_i.classList.toggle("is-hidden")
+}, false);
+naver_i_root.addEventListener('mouseover', function() {
+  naver_i.classList.toggle("is-hidden")
+}, false);
+
+naver_ii.addEventListener('mouseleave', function() {
+  naver_ii.classList.toggle("is-hidden")
+}, false);
+naver_ii_root.addEventListener('mouseleave', function() {
+  naver_ii.classList.toggle("is-hidden")
+}, false);
+naver_ii_root.addEventListener('mouseover', function() {
+  naver_ii.classList.toggle("is-hidden")
+}, false);
+
+naver_iii.addEventListener('mouseleave', function() {
+  naver_iii.classList.toggle("is-hidden")
+}, false);
+naver_iii_root.addEventListener('mouseleave', function() {
+  naver_iii.classList.toggle("is-hidden")
+}, false);
+naver_iii_root.addEventListener('mouseover', function() {
+  naver_iii.classList.toggle("is-hidden")
+}, false);
+
+
+naver_i.classList.toggle("is-hidden")
+
+naver_ii.classList.toggle("is-hidden")
+
+naver_iii.classList.toggle("is-hidden")*/
