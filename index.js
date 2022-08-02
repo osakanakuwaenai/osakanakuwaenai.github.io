4ee7eca29ca2a7fb933ad3a36392951f9d22c202
osakanakuwaenai <osakanakuwaenai@noreply.github.com>
2022-08-02 15:18:24 +0900

    jqueryなんて…元々導入しなかったんや…(泣)など

--- index.js
+++ index.js
@@ -1,45 +1,35 @@
-/*var naver_i = document.querySelector(".naver_i");
-var naver_i_root = document.querySelector(".naver_i_root");
-var naver_ii = document.querySelector(".naver_ii");
-var naver_ii_root = document.querySelector(".naver_ii_root");
-var naver_iii = document.querySelector(".naver_iii");
-var naver_iii_root = document.querySelector(".naver_iii_root");
-
-
-
-naver_i.addEventListener('mouseleave', function() {
-  naver_i.classList.toggle("is-hidden")
-}, false);
-naver_i_root.addEventListener('mouseleave', function() {
-  naver_i.classList.toggle("is-hidden")
-}, false);
-naver_i_root.addEventListener('mouseover', function() {
-  naver_i.classList.toggle("is-hidden")
-}, false);
-
-naver_ii.addEventListener('mouseleave', function() {
-  naver_ii.classList.toggle("is-hidden")
-}, false);
-naver_ii_root.addEventListener('mouseleave', function() {
-  naver_ii.classList.toggle("is-hidden")
-}, false);
-naver_ii_root.addEventListener('mouseover', function() {
-  naver_ii.classList.toggle("is-hidden")
-}, false);
-
-naver_iii.addEventListener('mouseleave', function() {
-  naver_iii.classList.toggle("is-hidden")
-}, false);
-naver_iii_root.addEventListener('mouseleave', function() {
-  naver_iii.classList.toggle("is-hidden")
-}, false);
-naver_iii_root.addEventListener('mouseover', function() {
-  naver_iii.classList.toggle("is-hidden")
-}, false);
-
-
-naver_i.classList.toggle("is-hidden")
-
-naver_ii.classList.toggle("is-hidden")
-
-naver_iii.classList.toggle("is-hidden")*/
+document.write(`
+<header>
+<ul>
+  <li><a href="//osakanakuwaenai.github.io"><nobr>&emsp;HOME&emsp;</nobr></a></li>
+  <li><a href="//osakanakuwaenai.github.io/programing"><nobr>&emsp;プログラミング&emsp;</nobr></a>
+    <ul>
+      <li><a href="//osakanakuwaenai.github.io/programing/js">Java Script</a></li>
+      <li><a href="//osakanakuwaenai.github.io/programing/scratch">Scratch</a></li>
+    </ul>
+  </li>
+  <li><a href="//osakanakuwaenai.github.io/minecraft"><nobr>&emsp;&emsp;マイクラ&emsp;&emsp;</nobr></a>
+    <ul>
+      <li><a href="//osakanakuwaenai.github.io/minecraft/building">建築</a></li>
+      <li><a href="//osakanakuwaenai.github.io/minecraft/redstone">赤石</a></li>
+      <li><a href="//osakanakuwaenai.github.io/minecraft/rtm">RealTrainMod</a></li>
+      <li><a href="//osakanakuwaenai.github.io/minecraft/others">その他のMOD</a></li>
+    </ul>
+  </li>
+  <li><a href="//osakanakuwaenai.github.io/music"><nobr>&emsp;&emsp;&emsp;作曲&emsp;&emsp;&emsp;</nobr></a>
+    <ul>
+      <li><a href="//osakanakuwaenai.github.io/music/undertale">UNDERTALE</a></li>
+      <li><a href="//osakanakuwaenai.github.io/music/original">オリジナルBGM</a></li>
+      <li><a href="//osakanakuwaenai.github.io/music/others">その他</a></li>
+    </ul>
+  </li>
+  <li><a href="//osakanakuwaenai.github.io/pictures"><nobr>&emsp;&emsp;&emsp;&emsp;絵&emsp;&emsp;&emsp;</nobr></a>
+    <ul>
+      <li><a href="//osakanakuwaenai.github.io/pictures/dots">ドット絵</a></li>
+      <li><a href="//osakanakuwaenai.github.io/pictures/others">その他の絵</a></li>
+    </ul>
+  </li>
+  <li><a href="https://forms.gle/VAdJjueZNdRPJi9aA"><nobr>&emsp;&emsp;制作依頼&emsp;&emsp;</nobr></a></li>
+</ul>
+</header>
+`);
