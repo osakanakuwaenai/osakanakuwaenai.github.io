6cf5921ace1d7049d2092c89f45017d0d09adf3a
osakanakuwaenai <osakanakuwaenai@noreply.github.com>
2022-08-11 15:42:52 +0900

    色々々々

--- index.js
+++ index.js
@@ -1,35 +1,57 @@
+'use strict';
 document.write(`
 <header>
-<ul>
-  <li><a href="//osakanakuwaenai.github.io"><nobr>&emsp;HOME&emsp;</nobr></a></li>
-  <li><a href="//osakanakuwaenai.github.io/programing"><nobr>&emsp;プログラミング&emsp;</nobr></a>
+  <nav>
     <ul>
-      <li><a href="//osakanakuwaenai.github.io/programing/js">Java Script</a></li>
-      <li><a href="//osakanakuwaenai.github.io/programing/scratch">Scratch</a></li>
+      <li><a href="//osakanakuwaenai.github.io"><nobr>&emsp;HOME&emsp;</nobr></a></li>
+      <li><a href="//osakanakuwaenai.github.io/programing"><nobr>&emsp;プログラミング&emsp;</nobr></a>
+        <ul>
+          <li><a href="//osakanakuwaenai.github.io/programing/js">Java Script</a></li>
+          <li><a href="//osakanakuwaenai.github.io/programing/scratch">Scratch</a></li>
+        </ul>
+      </li>
+      <li><a href="//osakanakuwaenai.github.io/minecraft"><nobr>&emsp;&emsp;マイクラ&emsp;&emsp;</nobr></a>
+        <ul>
+          <li><a href="//osakanakuwaenai.github.io/minecraft/building">建築</a></li>
+          <li><a href="//osakanakuwaenai.github.io/minecraft/redstone">赤石</a></li>
+          <li><a href="//osakanakuwaenai.github.io/minecraft/rtm">RealTrainMod</a></li>
+          <li><a href="//osakanakuwaenai.github.io/minecraft/others">その他のMOD</a></li>
+        </ul>
+      </li>
+      <li><a href="//osakanakuwaenai.github.io/music"><nobr>&emsp;&emsp;&emsp;作曲&emsp;&emsp;&emsp;</nobr></a>
+        <ul>
+          <li><a href="//osakanakuwaenai.github.io/music/undertale">UNDERTALE</a></li>
+          <li><a href="//osakanakuwaenai.github.io/music/original">オリジナルBGM</a></li>
+          <li><a href="//osakanakuwaenai.github.io/music/others">その他</a></li>
+        </ul>
+      </li>
+      <li><a href="//osakanakuwaenai.github.io/pictures"><nobr>&emsp;&emsp;&emsp;&emsp;絵&emsp;&emsp;&emsp;</nobr></a>
+        <ul>
+          <li><a href="//osakanakuwaenai.github.io/pictures/dots">ドット絵</a></li>
+          <li><a href="//osakanakuwaenai.github.io/pictures/others">その他の絵</a></li>
+        </ul>
+      </li>
+      <li><a href="https://forms.gle/VAdJjueZNdRPJi9aA"><nobr>&emsp;&emsp;制作依頼&emsp;&emsp;</nobr></a></li>
     </ul>
-  </li>
-  <li><a href="//osakanakuwaenai.github.io/minecraft"><nobr>&emsp;&emsp;マイクラ&emsp;&emsp;</nobr></a>
-    <ul>
-      <li><a href="//osakanakuwaenai.github.io/minecraft/building">建築</a></li>
-      <li><a href="//osakanakuwaenai.github.io/minecraft/redstone">赤石</a></li>
-      <li><a href="//osakanakuwaenai.github.io/minecraft/rtm">RealTrainMod</a></li>
-      <li><a href="//osakanakuwaenai.github.io/minecraft/others">その他のMOD</a></li>
-    </ul>
-  </li>
-  <li><a href="//osakanakuwaenai.github.io/music"><nobr>&emsp;&emsp;&emsp;作曲&emsp;&emsp;&emsp;</nobr></a>
-    <ul>
-      <li><a href="//osakanakuwaenai.github.io/music/undertale">UNDERTALE</a></li>
-      <li><a href="//osakanakuwaenai.github.io/music/original">オリジナルBGM</a></li>
-      <li><a href="//osakanakuwaenai.github.io/music/others">その他</a></li>
-    </ul>
-  </li>
-  <li><a href="//osakanakuwaenai.github.io/pictures"><nobr>&emsp;&emsp;&emsp;&emsp;絵&emsp;&emsp;&emsp;</nobr></a>
-    <ul>
-      <li><a href="//osakanakuwaenai.github.io/pictures/dots">ドット絵</a></li>
-      <li><a href="//osakanakuwaenai.github.io/pictures/others">その他の絵</a></li>
-    </ul>
-  </li>
-  <li><a href="https://forms.gle/VAdJjueZNdRPJi9aA"><nobr>&emsp;&emsp;制作依頼&emsp;&emsp;</nobr></a></li>
-</ul>
+  </nav>
 </header>
 `);
+const localUrl = "file:///Users/osakana/googen/googen/dandan/osakanakuwaenai.github.io/minecraft/others/withermod.html";
+console.log("process 1");
+
+fetch(localUrl)
+  .then((response) => {
+    if (response.ok) {
+      throw new Error("Error");
+    }
+    console.log(`got data from "${localUrl}"`);
+    return response.text();
+  })
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) =>{
+    console.error(error.message);
+  });
+
+console.log("sync process 2");
